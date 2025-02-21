434ff7f03cb11e184695c16016e680de4cf375b4
TanmoyDhar1077 <tanmoydhar1077@gmail.com>
2025-02-21 21:06:01 +0600

    Updated

--- README.md
+++ README.md
@@ -64,3 +64,8 @@ If you discover a security vulnerability within Laravel, please send an e-mail t
 ## License
 
 The Laravel framework is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
+
+## Version Information
+
+Laravel v11.43.0 
+PHP v8.2.12
--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title></title>
 </head>
+
 <body>
     <div class="container">
         <h1 class="text-center my-5">Category list</h1>
         <div class="row">
-            <table class="table table-secondary">
+            <table class="table table-success table-striped table-hover">
                 <thead>
                     <tr>
                         <th>ID</th>
@@ -26,20 +29,35 @@
                 <tbody>
                     @foreach ($categories as $category)
                         <tr>
-                            <td>{{ $loop -> iteration}}</td>
+                            <td>{{ $loop->iteration }}</td>
                             <td>{{ $category->name }}</td>
                             <td>{{ $category->description }}</td>
-                            <td>{{ $category->status }}</td>
-                            <td>{{ $category->created_at }}</td>
-                            <td>{{ $category->updated_at }}</td>
                             <td>
-                                Action
+                                <span class="badge {{ $category->status == 1 ? 'bg-success' : 'bg-danger' }}">
+                                    {{ $category->status == 1 ? 'Active' : 'Inactive' }}
+                                </span>
+                            </td>
+                            <td>{{ $category->created_at ? $category->created_at->format('d M, Y h:i:s A') : 'NULL' }}
+                            </td>
+                            <td>{{ $category->updated_at ? $category->updated_at->format('d M, Y h:i:s A') : 'NULL' }}
+                            </td>
+
+                            <td>
+                                <span class="d-flex gap-1">
+                                    <a href="" class="btn {{ $category->status == 1 ? 'btn-warning' : 'btn-success' }}">{{ $category->status == 1 ? 'Inactive' : 'Active' }}</a>
+                                    <a href="" class="btn btn-primary">Edit</a>
+                                    <a href="" class="btn btn-danger">Delete</a>
+                                </span>
                             </td>
                         </tr>
                     @endforeach
+                </tbody>
             </table>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
